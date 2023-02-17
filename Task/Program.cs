a221ca9ab64add56dcfba00b21f2110c0be89e54
ZakirovAndrey <zak.andrey94@gmail.com>
2023-02-17 16:18:47 +0300

    Написан код

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, 
+//либо *задать на старте выполнения алгоритма*. 
+
+string[] arrayStart = new string[] {"Russia", ":-)", "hello", "cat", "12345", "-2", "123"};
+string[] arrayFinal = new string[arrayStart.Length];
+
+void SecondArray(string[] arrayStart, string[] arrayFinal)
+{
+    int count = 0;
+    for (int i = 0; i < arrayStart.Length; i++)
+    {
+    if(arrayStart[i].Length <= 3)
+        {
+        arrayFinal[count] = arrayStart[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(arrayStart, arrayFinal);
+PrintArray(arrayFinal);
